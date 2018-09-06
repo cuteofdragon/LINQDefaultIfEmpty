0e1888face416ad225c46ed11ed0c6291f2eb4ac
oomusou <oomusou@gmail.com>
2018-09-06 21:23:43 +0800

    Imperative Style

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp
 {
@@ -6,7 +8,29 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var members = new List<Member>
+            {
+                new Member {Username = "Sam", Password = "1234"},
+                new Member {Username = "You", Password = "4567"}
+            };
+
+            var user = new Member {Username = "Sam", Password = "1234"};
+
+            var member = members
+                .FirstOrDefault(x => x.Username == user.Username && x.Password == user.Password);
+
+            string result;
+            
+            if (member != null)
+            {
+                result = member.Username;
+            }
+            else
+            {
+                result = "";
+            }
+            
+            Console.WriteLine($"Welcome {result}");
         }
     }
 }
